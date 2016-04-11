fbcf18f4bb2c2596001d9dca20718a62a205c079
ewhitmore <ewhitmor@gmail.com>
2016-04-11 12:20:22 -0400

    cleanup

---
+++ Enterprise.Persistence.Tests/IntegrationTests/ClassroomDaoIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enterprise.Persistence.Tests.IntegrationTests
+{
+    [TestClass]
+    public class ClassroomDaoIntegrationTests
+    {
+        // See TeacherDtoIntegrationTests for examples of how to do this
+    }
+}
---
+++ Enterprise.Persistence.Tests/IntegrationTests/StudentDaoIntegrationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enterprise.Persistence.Tests.IntegrationTests
+{
+    [TestClass]
+    public class StudentDaoIntegrationTests
+    {
+        // See TeacherDtoIntegrationTests for examples of how to do this
+
+    }
+}
