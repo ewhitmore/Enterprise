b88c4c16d6983984e99b32272a0467e49285788c
ewhitmore <ewhitmor@gmail.com>
2016-04-11 15:33:53 -0400

    Jasmine/Resharper Unit testing layout complete

---
+++ Enterprise.Web/app/teacher/teacher.test.ts
@@ -0,0 +1,22 @@
+﻿///<reference path="../../Scripts/typings/jasmine/jasmine.d.ts"/>
+///<reference path="teacherDto.ts"/>
+
+module App.Teacher {
+    'use strict';
+
+    describe("TeacherDto", () => {
+
+        var teacher: ITeacher;
+
+        beforeEach(() => {
+        
+        });
+
+
+        it("should pass", () => {
+            var x = 1;
+            expect(x).not.toBeNull();
+        });
+    });
+}
+
---
+++ Enterprise.Web/app/teacher/teacherDto.ts
@@ -0,0 +1,18 @@
+﻿module App.Teacher {
+
+    export interface ITeacher {
+        getFullName():string;
+    }
+
+    class Teacher implements ITeacher {
+
+        private id: number;
+        private fullname: string;
+        private birthday: Date;
+
+        getFullName(): string {
+            return this.fullname;
+        }
+    }
+
+}
